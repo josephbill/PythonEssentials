7fe1fbeb8ccc5003ea8875575553fefac5c8b729
josephbill <josephbill00@gmail.com>
2023-05-24 13:31:32 +0300

    added DSA package

--- python_pip.py
+++ python_pip.py
@@ -1,6 +1,6 @@
 # PIP  : package manager for python packages /  modules
 # Python Packages /Modules  : ready code / code library /
-# benefits : file containg a set of functions you might want to use in your application.
+# benefits : file containing a set of functions you might want to use in your application.
 # if the modules are not built in like pip. We simply download the package
 # pip install packagename
 # pip install camelcase
---
+++ tests.py
@@ -0,0 +1,7 @@
+class Pet:
+
+  def __init__(self, name):
+    self.name = name
+
+buddy = Pet("Buddy")
+print(buddy.name)
